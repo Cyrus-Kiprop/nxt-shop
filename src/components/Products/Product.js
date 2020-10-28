6ea20ecc2649e80ee40be720b421e15eec949403
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-10-28 15:08:37 +0300

    add Product page component

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { ProductConsumer } from "../../context";
 
 export default function Product({
-  product: { id, title, img, price, incart },
+  product: { id, title, img, price, inCart },
 }) {
   return (
     <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
@@ -16,10 +16,32 @@ export default function Product({
           <Link to="/details">
             <img src={img} alt="product-img" className="card-img-top" />
           </Link>
+
+          <button
+            className="cart-btn"
+            disabled={inCart ? true : false}
+            onClick={() => {
+              console.log("added to the cart");
+            }}
+          >
+            {inCart ? (
+              <p className="text-capitalize mb-0">in Cart</p>
+            ) : (
+              <i className="fas fa-cart-plus" />
+            )}
+          </button>
+        </div>
+        <div className="card-footer d-flex justify-content-between">
+          <p className="align-self-center mb-0"> {title} </p>
+          <h5 className="text-blue font-italic mb-0">{price}</h5>
         </div>
       </div>
     </ProductWrapper>
   );
 }
 
-const ProductWrapper = styled.div``;
+const ProductWrapper = styled.div`
+  .card {
+    border-color: transparent;
+  }
+`;
