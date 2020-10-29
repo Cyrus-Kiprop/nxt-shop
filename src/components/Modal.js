a3737282d5a3e453db39d95979c46b39929dc98b
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-10-29 13:33:04 +0300

    style the product modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import { ButtonContainer } from "./Shared/Button";
-import { ProdcutConsumer } from "../context";
+import { ProductConsumer } from "../context";
 
 export default function Modal() {
   return (
@@ -14,20 +15,37 @@ export default function Modal() {
         if (!modalOpen) {
           return null;
         } else {
-          <modalContainer>
-            <div className="container">
-              <div calssName="row">
-                <div
-                  id="modal "
-                  className="col-8 mx-auto col-md col-lg-4 text-center text-capitalize"
-                >
-                  <h5> item add to the cart</h5>
+          return (
+            <ModalContainer>
+              <div className="container">
+                <div calssName="row">
+                  <div
+                    id="modal "
+                    className="col-8 mx-auto col-md col-lg-4 text-center text-capitalize"
+                  >
+                    <h5> item added to the cart</h5>
+                  </div>
                 </div>
               </div>
-            </div>
-          </modalContainer>;
+            </ModalContainer>
+          );
         }
       }}
     </ProductConsumer>
   );
 }
+
+const ModalContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0,0,0,0.3)
+  display: flex;
+  align-items: center;
+  justify-contents: center;
+  #modal {
+      background: var(--mainWhite)
+  }
+`;
